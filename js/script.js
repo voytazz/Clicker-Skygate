87ef07f3d2e11807e3c889d5cfc9a54be5a94f5e
voytazz <kubawoj96@gmail.com>
2017-12-06 18:22:25 +0100

    all bonus

--- js/script.js
+++ js/script.js
@@ -1,38 +1,86 @@
-document.addEventListener("DOMContentLoaded", function() { 
+document.addEventListener("DOMContentLoaded", function () {
 
     var cookie = document.querySelector(".cookieImg");
     var cookieCounter = document.querySelector(".cookieCounter");
     var cursorBonus = document.querySelector(".cursorBonus");
+    var grandmaBonus = document.querySelector(".grandmaBonus");
+    var farmBonus = document.querySelector(".farmBonus");
+    var bakeryBonus = document.querySelector(".bakeryBonus");
+    var mineBonus = document.querySelector(".mineBonus");
     var counter = 0;
-    
-    cookie.addEventListener("click", function() {
+
+    cookie.addEventListener("click", function () {
         counter++;
         cookieCounter.innerHTML = counter;
     })
-    
 
-        
-
-    cursorBonus.addEventListener("click", function() {
-
-        if(counter >= 20) {
-    var myiInterval = setInterval(function(){      
-        function Count(bonus) {
+    function Count(bonus) {
         this.bonus = bonus;
         this.bonusCount = function () {
             cookieCounter.innerHTML = counter += this.bonus;
-            
         }
     }
-    var kursor = new Count(1);
-    kursor.bonusCount(); }, 1000);
-            counter-=20;
-        } else {
-            
-        }
-        
+
+
+    cursorBonus.addEventListener("click", function () {
+
+        if (counter >= 20) {
+            counter -= 20;
+
+            setInterval(function () {
+
+                var kursor = new Count(1);
+                kursor.bonusCount();
+            }, 1000);
+
+        } else {}
+
     })
-    
-    
-    
+
+    grandmaBonus.addEventListener("click", function () {
+        if (counter >= 30) {
+            counter -= 30;
+            setInterval(function () {
+                var grandma = new Count(5);
+                grandma.bonusCount();
+            }, 1000);
+        }
+
+    })
+
+    farmBonus.addEventListener("click", function () {
+        if (counter >= 50) {
+            counter -= 50;
+            setInterval(function () {
+                var farm = new Count(10);
+                farm.bonusCount();
+            }, 1000);
+        }
+
+    })
+
+    bakeryBonus.addEventListener("click", function () {
+        if (counter >= 100) {
+            counter -= 100;
+            setInterval(function () {
+                var bakery = new Count(15);
+                bakery.bonusCount();
+            }, 1000);
+        }
+
+    })
+
+    mineBonus.addEventListener("click", function () {
+        if (counter >= 500) {
+            counter -= 500;
+            setInterval(function () {
+                var mine = new Count(20);
+                mine.bonusCount();
+            }, 1000);
+        }
+
+    })
+
+
+
 })
