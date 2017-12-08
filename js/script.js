55f51665bdb555f3bdadaff1515f9fac3ceeb2b4
voytazz <kubawoj96@gmail.com>
2017-12-08 17:12:57 +0100

    layout fix

--- js/script.js
+++ js/script.js
@@ -13,6 +13,7 @@ document.addEventListener("DOMContentLoaded", function () {
     var mineBonus = document.querySelector(".mineBonus");
     var mineCounter = document.querySelector(".mineCounter");
     var cookiesOnSecond = document.querySelector(".cookiesOnSecond");
+    
     var counter = 0;
     var bonusCookiesCounter = 0;
 
@@ -25,15 +26,33 @@ document.addEventListener("DOMContentLoaded", function () {
 
     setInterval(function () {
         if (counter >= cursorCost) {
-            cursorBonus.style.backgroundColor = "#1D2135";
+            cursorBonus.style.opacity = "1";
         } else {
-            cursorBonus.style.backgroundColor = "rgba(109, 130, 151, 0.3)";
+            cursorBonus.style.opacity = "0.3";
         }
 
         if (counter >= grandmaCost) {
-            grandmaBonus.style.backgroundColor = "#8BA9E1";
+            grandmaBonus.style.opacity = "1";
         } else {
-            grandmaBonus.style.backgroundColor = "lightslategray";
+            grandmaBonus.style.opacity = "0.3";
+        }
+
+        if (counter >= farmCost) {
+            farmBonus.style.opacity = "1";
+        } else {
+            farmBonus.style.opacity = "0.3";
+        }
+
+        if (counter >= bakeryCost) {
+            bakeryBonus.style.opacity = "1";
+        } else {
+            bakeryBonus.style.opacity = "0.3";
+        }
+
+        if (counter >= mineCost) {
+            mineBonus.style.opacity = "1";
+        } else {
+            mineBonus.style.opacity = "0.3";
         }
 
 
@@ -59,71 +78,71 @@ document.addEventListener("DOMContentLoaded", function () {
             bonusCookiesCounter += 0.2;
             cursorCounter.innerHTML = cursorCost;
             cookiesOnSecond.innerHTML = bonusCookiesCounter.toFixed(2);
-            ownedCursor+=1;
+            ownedCursor += 1;
             var kursor = new Count(1);
-            kursor.bonusCount();
+                kursor.bonusCount();
             setInterval(function () {
                 var kursor = new Count(1);
                 kursor.bonusCount();
+                
 
             }, 5000);
 
         }
     })
-    
-    
+
+
     var grandmaCost = 100;
     var ownedGrandma = 0;
     grandmaBonus.addEventListener("click", function () {
         if (counter >= grandmaCost) {
             counter -= grandmaCost;
             grandmaCost += 10;
-            bonusCookiesCounter += 1.25;
+            bonusCookiesCounter += 1;
             grandmaCounter.innerHTML = grandmaCost;
             cookiesOnSecond.innerHTML = bonusCookiesCounter.toFixed(2);
-            ownedGrandma+=1;
-            var grandma = new Count(5);
-            grandma.bonusCount();
+            ownedGrandma += 1;
+            
             setInterval(function () {
-                var grandma = new Count(5);
+                var grandma = new Count(1);
                 grandma.bonusCount();
-            }, 4000);
+            }, 1000);
         }
 
     })
 
     var farmCost = 500;
+    var ownedFarm = 0;
     farmBonus.addEventListener("click", function () {
         if (counter >= farmCost) {
             counter -= farmCost;
             farmCost += 50;
-            bonusCookiesCounter += 3.33;
+            bonusCookiesCounter += 3;
             farmCounter.innerHTML = farmCost;
             cookiesOnSecond.innerHTML = bonusCookiesCounter.toFixed(2);
-            var farm = new Count(10);
-            farm.bonusCount();
+            
             setInterval(function () {
-                var farm = new Count(10);
+                var farm = new Count(3);
                 farm.bonusCount();
-            }, 3000);
+            }, 1000);
         }
 
     })
 
     var bakeryCost = 2000;
+    var ownedBakery = 0;
     bakeryBonus.addEventListener("click", function () {
         if (counter >= bakeryCost) {
             counter -= bakeryCost;
             bakeryCost += 1000;
-            bonusCookiesCounter += 7.5;
+            bonusCookiesCounter += 6;
             bakeryCounter.innerHTML = bakeryCost;
             cookiesOnSecond.innerHTML = bonusCookiesCounter.toFixed(2);
-            var bakery = new Count(15);
-            bakery.bonusCount();
+          
             setInterval(function () {
-                var bakery = new Count(15);
+                var bakery = new Count(6);
                 bakery.bonusCount();
-            }, 2000);
+            }, 1000);
         }
 
     })
@@ -133,13 +152,12 @@ document.addEventListener("DOMContentLoaded", function () {
         if (counter >= mineCost) {
             counter -= mineCost;
             mineCost += 1000;
-            bonusCookiesCounter += 20;
+            bonusCookiesCounter += 15;
             mineCounter.innerHTML = mineCost;
             cookiesOnSecond.innerHTML = bonusCookiesCounter.toFixed(2);
-            var mine = new Count(20);
-            mine.bonusCount();
+            
             setInterval(function () {
-                var mine = new Count(20);
+                var mine = new Count(15);
                 mine.bonusCount();
             }, 1000);
         }
